d5a60b1e81e8748b8858dd0c4bc15ae5f00a68e1
29438 <47177190+29438@users.noreply.github.com>
2022-01-27 13:16:12 +0800

    Add files via upload

--- actions/teacherSign.py
+++ actions/teacherSign.py
@@ -114,15 +114,16 @@ class teacherSign:
             "deviceId": self.userInfo['deviceId']
         }
 
-        self.cpdailyExtension = CT.encrypt_CpdailyExtension(
+        self.cpdailyExtension = CpdailyTools.encrypt_CpdailyExtension(
             json.dumps(extension))
 
-        self.bodyString = CT.encrypt_BodyString(json.dumps(self.form))
+        self.bodyString = CpdailyTools.encrypt_BodyString(
+            json.dumps(self.form))
 
         self.submitData = {
             "lon": self.userInfo['lon'],
-            "version": "first_v2",
-            "calVersion": "firstv",
+            "version": self.userInfo['signVersion'],
+            "calVersion": self.userInfo['calVersion'],
             "deviceId": self.userInfo['deviceId'],
             "userId": self.userInfo['username'],
             "systemName": self.userInfo['systemName'],
@@ -133,10 +134,7 @@ class teacherSign:
             "model": self.userInfo['model'],
         }
 
-        sign = ''.join("%s=%s&" % (i, self.submitData[i]) for i in [
-                       "appVersion", "bodyString", "deviceId", "lat", "lon", "model", "systemName", "systemVersion", "userId"]) + "ytUQ7l2ZZu8mLvJZ"
-        sign = HSF.strHash(sign, 5)
-        self.submitData['sign'] = sign
+        self.submitData['sign'] = CpdailyTools.signAbstract(self.submitData)
 
     # 提交签到信息
     def submitForm(self):
